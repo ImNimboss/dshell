b1986dffb29c8f45861c6336806b3124772cf1d3
Nimboss <nimit.grover24@gmail.com>
2022-01-18 22:09:26 +0530

    Changed version number because PyPI wont let me edit the damn readme

--- dshell/__init__.py
+++ dshell/__init__.py
@@ -2,7 +2,7 @@ from dshell.cog import DShell, setup
 
 __author__: str = 'ImNimboss'
 __license__: str = 'MIT'
-__version__: str = '0.0.1'
+__version__: str = '0.0.2'
 GITHUB: str = 'https://github.com/ImNimboss/dshell'
 ISSUE_TRACKER: str = 'https://github.com/ImNimboss/dshell/issues'
 DOCUMENTATION: str = 'https://github.com/ImNimboss/dshell/tree/main/Documentation'
