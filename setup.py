49f36598e50b2ec750c5a41833fdc8168aebcc6c
Nimboss <nimit.grover24@gmail.com>
2022-01-18 17:37:21 +0530

    Whoops forgot a comma

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: Implementation :: CPython'
+        'Programming Language :: Python :: Implementation :: CPython',
         'Topic :: Communications',
         'Topic :: Communications :: Chat',
         'Topic :: Internet',
