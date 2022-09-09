987add210a9fc7c2e6dd6acc5352d36d57244210
ImNimboss <nim@nimboss.me>
2022-09-09 15:06:08 +0530

    Version number

--- dshell/__init__.py
+++ dshell/__init__.py
@@ -2,7 +2,7 @@ from dshell.cog import DShell, setup
 
 __author__: str = 'ImNimboss'
 __license__: str = 'MIT'
-__version__: str = '0.0.2'
+__version__: str = '0.0.3'
 GITHUB: str = 'https://github.com/ImNimboss/dshell'
 ISSUE_TRACKER: str = 'https://github.com/ImNimboss/dshell/issues'
 DOCUMENTATION: str = 'https://github.com/ImNimboss/dshell/tree/main/Documentation'
